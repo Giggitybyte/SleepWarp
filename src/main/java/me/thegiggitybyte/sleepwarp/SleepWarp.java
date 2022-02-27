f7e5ad414e6f840bf3b267b52bc3027ccad646c4
Luke Triano <luketriano115@gmail.com>
2022-02-27 04:31:31 -0800

    docs: improved wording in default config

--- src/main/java/me/thegiggitybyte/sleepwarp/SleepWarp.java
+++ src/main/java/me/thegiggitybyte/sleepwarp/SleepWarp.java
@@ -24,27 +24,26 @@ public class SleepWarp implements ModInitializer {
                 # Valid: 1 - 2147483647 | Default: 60
                 maxTimeAdded=60
                 
-                # Scales time warp speed in relation to the amount of players currently sleeping.
+                # Scales time warp speed in relation to the amount of players sleeping.
                 # Lower values will require more players to bring the warp up to full speed, and vice versa.
                 # Valid: 0.1 - 1.0 | Default: 0.2
                 accelerationCurve=0.2
-                
-                # When true, block entities (e.g. furnaces, spawners, pistons) will be ticked at the same speed as the time warp to
-                # simulate the passage of time. This option may cause performance issues, especially on servers with high activity.
-                # Full list of affected blocks: https://minecraft.fandom.com/wiki/Block_entity#List_of_block_entities
-                # Valid: true, false | Default: false
-                tickBlockEntities=false
-                
-                # When true, chunks will be ticked at the same speed as the time warp to simulate the passage of time. Because chunk ticks are responsible for most
-                # of the world simulation (e.g. mob spawns, crop growth) this option *will* cause lag on populated servers or servers with high chunk view distances.
-                # Full list of affected parts of the world: https://minecraft.fandom.com/wiki/Tick#Chunk_tick
-                # For best performance with this option enabled, reduce maxTimeAdded above and download Lithium: https://modrinth.com/mod/lithium
-                tickChunks=false
                 
                 # When true, the 'playersSleepingPercentage' gamerule will be respected and a percentage of players must sleep to begin the time warp.
                 # When false, only one player will be required to begin accelerating time.
                 # Valid: true, false | Default: false
                 useSleepPercentage=false
+                
+                # When true, block entities (e.g. furnaces, spawners, pistons) will be ticked at the same rate as the time warp to simulate the passage of time.
+                # This feature can cause performance issues during the time warp, especially in worlds with high amounts of block entities.
+                # Valid: true, false | Default: false
+                tickBlockEntities=true
+                
+                # When true, chunks will be ticked at the same rate as the time warp to simulate the passage of time. This is a demanding feature since a chunk tick
+                # is responsible for most of the world simulation (e.g. crop growth, fire spread, mob spawns). Servers with high player counts or high chunk view
+                # distances should decrease maxTimeAdded and download Lithium (https://modrinth.com/mod/lithium) for the best performance with this feature enabled.
+                # Valid: true, false | Default: false
+                tickChunks=true
                 """;
     }
 }
